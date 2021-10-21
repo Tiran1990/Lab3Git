0c4c78df5415a38bba9cb4f07667e42b040cd63f
Tiran1990 <t.hesawi1990@gmail.com>
2021-10-21 20:42:35 +0300

    first commit

--- src/SharedData.java
+++ src/SharedData.java
@@ -1,37 +1,37 @@
-public class SharedData 
+public class SharedData // hey
 {
 	private int [] array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
-	
+
 	public SharedData(int[] array, int b) {
-		
+
 		this.array = array;
 		this.b = b;
 	}
 
-	public boolean[] getWinArray() 
+	public boolean[] getWinArray()
 	{
 		return winArray;
 	}
 
-	public void setWinArray(boolean [] winArray) 
+	public void setWinArray(boolean [] winArray)
 	{
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	public int[] getArray()
 	{
 		return array;
 	}
 
-	public int getB() 
+	public int getB()
 	{
 		return b;
 	}
 
-	public boolean getFlag() 
+	public boolean getFlag()
 	{
 		return flag;
 	}
